5c3dbe07b2b82271ac6f85f30df4b7bde8bae1bf
daniell <daniell@kali.kali>
2025-06-01 13:03:31 -0500

    [feat]: test for expense manager implemented.

--- src/core/enums/expense_type.py
+++ src/core/enums/expense_type.py
@@ -1,9 +1,6 @@
 from enum import Enum
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 0c16acf188a99d26a380ec15fd3a8fa1bc0bdf6c
 class ExpenseType(Enum):
     """
     Enum representing different types of expenses.

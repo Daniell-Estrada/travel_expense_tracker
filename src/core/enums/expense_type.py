0c16acf188a99d26a380ec15fd3a8fa1bc0bdf6c
NaghellyM <naghellymoreno0610@gmail.com>
2025-06-01 12:57:42 -0500

    feat: implementation at the domain and infrastructure layer

---
+++ src/core/enums/expense_type.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class ExpenseType(Enum):
+    """
+    Enum representing different types of expenses.
+    """
+
+    TRANSPORTATION = "Transportation"
+    ACCOMMODATION = "Accommodation"
+    FOOD = "Food"
+    ENTERTAINMENT = "Entertainment"
+    SHOPPING = "Shopping"
+    OTHER = "Other"
+
+    def __str__(self):
+        return self.value
---
+++ src/core/services/expense_manager.py
@@ -0,0 +1,86 @@
+from datetime import date
+from uuid import UUID, uuid4
+
+from src.application.dto import ExpenseDTO
+from src.core.domain import Expense
+from src.core.exceptions import InactiveTripError
+from src.core.interfaces import CurrencyConverter
+from src.core.interfaces.repositories import ExpenseRepository, TripRepository
+
+
+class ExpenseManager:
+    """
+    Manages expenses for trips, including registering expenses
+    and calculating daily budget differences.
+    """
+
+    def __init__(
+        self,
+        expense_repository: ExpenseRepository,
+        trip_repository: TripRepository,
+        currency_converter: CurrencyConverter,
+    ) -> None:
+        """
+        Initializes the ExpenseManager with repositories and a currency converter.
+            :param expense_repository: Repository for managing expenses.
+            :param trip_repository: Repository for managing trips.
+            :param currency_converter: Service for converting currencies.
+        """
+        self._expense_repository: ExpenseRepository = expense_repository
+        self._trip_repository: TripRepository = trip_repository
+        self._currency_converter: CurrencyConverter = currency_converter
+
+    def register_expense(self, expense_dto: ExpenseDTO) -> float:
+        """
+        Registers a new expense for a trip and calculates the daily budget difference.
+            :param expense_dto: Data Transfer Object containing expense details.
+            :return: The daily budget difference after registering the expense.
+            Raises:
+            InactiveTripError: If the trip associated with the expense is not active.
+        """
+        trip = self._trip_repository.get_by_id(expense_dto.trip_id)
+
+        if trip and not trip.is_active():
+            raise InactiveTripError()
+
+        expense = Expense(
+            expense_id=uuid4(),
+            trip_id=expense_dto.trip_id,
+            expense_date=expense_dto.expense_date,
+            original_amount=expense_dto.amount,
+            payment_method=expense_dto.payment_method,
+            expense_type=expense_dto.expense_type,
+        )
+
+        if trip.is_international:
+            converted_amount = self._currency_converter.convert(
+                expense_dto.amount, trip.currency, "COP"
+            )
+            expense.converted_amount_cop = converted_amount
+        else:
+            expense.converted_amount_cop = expense_dto.amount
+
+        self._expense_repository.save(expense)
+        return self.calculate_daily_difference(expense.trip_id, expense.expense_date)
+
+    def calculate_daily_difference(self, trip_id: UUID, expense_date: date) -> float:
+        """
+        Calculates the daily budget difference for a trip on a specific date.
+            :param trip_id: Unique identifier for the trip.
+            :param expense_date: Date for which to calculate the daily budget difference.
+            :return: The difference between the daily budget and total expenses for that date.
+            Raises:
+            InactiveTripError: If the trip is not active.
+        """
+        trip = self._trip_repository.get_by_id(trip_id)
+
+        if not trip or not trip.is_active():
+            raise InactiveTripError()
+
+        daily_budget = trip.daily_budget
+        expenses = self._expense_repository.get_by_trip_and_date(trip_id, expense_date)
+
+        total_expenses = sum(expense.converted_amount_cop for expense in expenses)
+        daily_difference = daily_budget - total_expenses
+
+        return daily_difference
